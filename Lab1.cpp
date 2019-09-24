366b4c3a3f182b21125d761e9e0eb3ae6b433d63
FairuzDiana <55312572+FairuzDiana@users.noreply.github.com>
2019-09-24 10:54:13 +0800

    Add files via upload

---
+++ Lab1.cpp
@@ -0,0 +1,43 @@
+//Name : fairuz Diana binti hamzah. ID: B031910307
+
+//Exercises 1.3
+
+//Task 1
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "University : Universiti Teknikal Malaysia, Melaka \n";
+    cout << "Faculty    : FTMK \n";
+    cout << "Program    : BITI \n\n\n";
+    cout << "My name is Fairuz Diana Binti Hamzah. I am from Tangkak, Johor\n\n\n\n";
+
+
+//Task 2(a)
+
+    cout << "******************************************************\n";
+    cout << "***********\t\tUTEM\t\t**************\n";
+    cout << "******************************************************\n\n";
+
+
+//Task 2(b)
+
+    cout << "10\n";
+    cout << "9\n";
+    cout << "8\n";
+    cout << "7           x\n";
+    cout << "6 x\n";
+    cout << "5                    x\n";
+    cout << "4   x\n";
+    cout << "3               x\n";
+    cout << "2     x\n";
+    cout << "1 x\n";
+    cout << "0 1 2 3 4 5 6 7 8 9 10\n";
+
+	Return(0);
+
+
+}
